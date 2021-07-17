e204efe95da39a655ce40ba211c18cfbcc93d13a
cdpadillae <cdpadillae@unal.edu.co>
2021-07-16 23:54:04 -0500

    Two functions that calculate areas

---
+++ area_01.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main(){
+	double PI = 3.1415926535897932;
+	double R  = 4.0;
+
+	std::cout <<"Perimeter=" << 2.0*PI*R << "\n";
+	std::cout <<"Area=     " << PI*R*R   << "\n";
+	std::cout << 2/4     << "\n";
+	std::cout << 2.0/4.0 << "\n";
+}
